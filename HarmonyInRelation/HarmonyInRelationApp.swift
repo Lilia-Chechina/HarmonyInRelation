3f5b6ed4788388c0ad85511cb8045aaa0bc70593
Lilia-Chechina <lilya-chechina@mail.ru>
2025-04-10 19:56:25 +0300

    Initial Commit

---
+++ HarmonyInRelation/HarmonyInRelationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonyInRelationApp.swift
+//  HarmonyInRelation
+//
+//  Created by Lilia Chechina on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct HarmonyInRelationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
