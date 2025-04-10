9bf73f69f491c5b2b9884d97875131af3ca72d96
Lilia-Chechina <lilya-chechina@mail.ru>
2025-04-10 21:02:15 +0300

    feat/ContentView/adding_animation+text

--- HarmonyInRelation/ContentView.swift
+++ HarmonyInRelation/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var scale: CGFloat = 1.0 // Начальный масштаб
+    @State private var opacity: Double = 1.0 // Начальная прозрачность
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.orange)
+                .scaleEffect(scale)  // масштаб
+                .opacity(opacity)  // прозрачность
+            // onAppear – for animation
+                .scaleEffect(5) // увеличиваем масштаб
+                .onAppear {
+                    withAnimation(
+                        Animation.easeInOut(duration: 1.5)
+                        /*
+                         тип анимации: анимация начинаетсяя медленно, ускоряться в середине и затем замедлятся в конце. Это делает переход более плавным и естественным, так как анимация не будет иметь резких изменений в скорости.
+                         */
+                            .repeatForever(autoreverses: true) // 1) бесконечно повтооряется 2) будет автоматически возвращаться к начальной точке после цикла
+                    ) {
+                        scale = 1.2 // Увеличиваем размер на 20%
+                        opacity = 0.7 // Уменьшаем прозрачность до 70%
+                    }
+                }
+            
+            Text("Сохраните любовь")
+                .bold()
+                .font(.system(size: 30))
+                .padding(.top, 55)
         }
-        .padding()
+        .padding() // отсуп для VStack
     }
 }
 
